56089af9964b367b875f959698974340819f5beb
AlexanderCleasby <cleasby.alex@gmail.com>
2018-10-16 03:23:48 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.each_with_index{|elem,i|
     cheese_types.each{|cheese|
-    if elem.include(cheese)
+    if elem.include?(cheese)
       return i
     end}
   }
