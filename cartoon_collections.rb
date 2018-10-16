34d44e5e630dd34f04414123752dca289f93d71c
AlexanderCleasby <cleasby.alex@gmail.com>
2018-10-16 03:13:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+
+def roll_call_dwarves(array)
+  #there is no diffrence between map and collect in behavior.
+  array.each_with_index{ |dwarf,i| puts " #{i+1}.#{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.each_with_index.collect{ |elem,i| "#{elem[0].upcase}#{elem[1..-1]}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.length>4
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each_with_index{|elem,i|
+  if cheese_types.incudes(elem)
+    return i
+  end}
+
+
 end
+#puts summon_captain_planet(["axe", "earth", "wind", "fire", "water", "heart"])
+#binding.pry
